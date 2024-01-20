5a00816e97421f44175178e1d67faf04921e5d1e
Ioana Van den Berghe <ioana.vandenberghe@viso-cor-mariae.be>
2024-01-20 13:17:20 +0100

    Chechboxes aangemaakt voor de tekstklaar aan te passen.

--- notepad/MainWindow.xaml.cs
+++ notepad/MainWindow.xaml.cs
@@ -19,6 +19,137 @@ namespace notepad
         public MainWindow()
         {
             InitializeComponent();
+
+
+
         }
+
+        private void btn_addnew_Click(object sender, RoutedEventArgs e)
+        {
+            txtb_invoeren.Focus();
+        }
+
+        private void btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            txtb_invoeren.Text = string.Empty;
+        }
+
+        private void chckb_bolt_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chckb_bolt.IsChecked == true)
+            {
+
+                txtb_invoeren.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+
+                txtb_invoeren.FontWeight = FontWeights.Normal;
+            }
+        }
+
+        private void chckb_italian_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chckb_italian.IsChecked == true)
+
+            {
+                txtb_invoeren.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                txtb_invoeren.FontStyle = FontStyles.Normal;
+            }
+
+        }
+
+        private void chckb_Underlined_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chckb_Underlined.IsChecked == true)
+            {
+
+                txtb_invoeren.TextDecorations = TextDecorations.Underline;
+            }
+            else
+            {
+
+                txtb_invoeren.TextDecorations = null;
+            }
+        }
+
+        private void chckb_red_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chckb_red.IsChecked == true)
+            {
+                txtb_invoeren.Foreground = Brushes.Red;
+            }
+            else
+            {
+                txtb_invoeren.ClearValue(TextBlock.ForegroundProperty);
+            }
+
+        }
+
+        private void chckb_orange_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chckb_orange.IsChecked == true)
+            {
+                txtb_invoeren.Foreground = Brushes.Red;
+            }
+            else
+            {
+                txtb_invoeren.ClearValue(TextBlock.ForegroundProperty);
+            }
+
+        }
+
+        private void chckb_yellow_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chckb_yellow.IsChecked == true)
+            {
+                txtb_invoeren.Foreground = Brushes.Red;
+            }
+            else
+            {
+                txtb_invoeren.ClearValue(TextBlock.ForegroundProperty);
+            }
+        }
+
+        private void chckb_green_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chckb_green.IsChecked == true)
+            {
+                txtb_invoeren.Foreground = Brushes.Red;
+            }
+            else
+            {
+                txtb_invoeren.ClearValue(TextBlock.ForegroundProperty);
+            }
+        }
+
+        private void chckb_blue_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chckb_blue.IsChecked == true)
+            {
+                txtb_invoeren.Foreground = Brushes.Red;
+            }
+            else
+            {
+                txtb_invoeren.ClearValue(TextBlock.ForegroundProperty);
+            }
+        }
+
+        private void chckb_purple_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chckb_purple.IsChecked == true)
+            {
+                txtb_invoeren.Foreground = Brushes.Purple;
+            }
+            else
+            {
+                txtb_invoeren.ClearValue(TextBlock.ForegroundProperty);
+            }
+        }
+
+        
     }
 }
